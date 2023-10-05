4f2cf8d9986e00305b4139df6e3bdf06aa6f0e1a
beny <benyyoulove@gmail.com>
2023-10-05 13:54:59 +0300

    start of event handel

--- src/modules/events/controllers/events.controller.ts
+++ src/modules/events/controllers/events.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { EventsService } from '../services/events.service';
+import { Request , Response} from 'express';
+import { EventInterface } from 'src/interfaces/event';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+
+    constructor(private EventsService:EventsService){}
+
+    @Post("register")
+    async registerEvent(
+        @Req() req : Request,
+        @Res() res: Response,
+        @Body() eventData: EventInterface, 
+    ){
+        try{
+
+        }catch(err){
+            console.log(err);
+            res.status(HttpStatus.BAD_REQUEST).json({msg:err.message}).end();
+        }
+    }
+
+}
--- src/schemas/event.schema.ts
+++ src/schemas/event.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 import { Date } from './date.schema';
 
 export type UserDocument = Document;
@@ -27,3 +27,22 @@ export class Event {
 };
 
 export const eventSchema = SchemaFactory.createForClass(Event);
+
+const EventModel = mongoose.model("User",eventSchema);
+
+// Function to get all events from DB
+export const getEvents = () => EventModel.find();
+
+// Function to get single event by id from DB
+export const getEventById = (id: string) => EventModel.findById(id);
+
+// Function to create a new event in DB
+export const createEvent = async (values: Record<string, any>) => {
+    return await new EventModel(values).save().then((event) => event.toObject());
+};
+
+// Function to delete an event from DB
+export const deleteEvent = (id: string) => EventModel.findOneAndDelete({ _id: id });
+
+// Function to find a single event and update it in the DB
+export const updateEventById = (id: string, values: Record<string, any>) => EventModel.findByIdAndUpdate(id, values);
